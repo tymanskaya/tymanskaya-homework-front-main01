95b91ac08fda1ef8ddbe4312619b231643bf1628
ekaterinapupeka <etymanskaya@bk.ru>
2024-03-25 16:41:10 +0300

    first commit

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -20,13 +20,12 @@ export const UserList = (props: UserListPropsType) => {
   return (
       <div id={'hw01-users'}>
         <h2>User List:</h2>
-
         <ul>
           {props.users.map((user=> {
             return(
-                <li id={`hw01-user-${user.id}`}>
+                <li key={user.id} id={`hw01-user-${user.id}`}>
                   <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
-                  {user.address.street} {user.address.city}
+                  {user.address.street}, {user.address.city}
                 </li>
             );
           }))}
