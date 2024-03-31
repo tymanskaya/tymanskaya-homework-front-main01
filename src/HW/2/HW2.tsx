fe84d667b7652c0ac22ad973a201bfb571a1d8c9
ekaterinapupeka <etymanskaya@bk.ru>
2024-03-31 23:50:05 +0300

    first commit

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -10,11 +10,11 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType;
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: UserType[]; // пропиши типизацию
 };
 
 export const HW2 = () => {
@@ -46,14 +46,14 @@ export const HW2 = () => {
 
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
-  const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+const filterUsers = () => {
+    const filteredUsers =currentUsers.myFriends.filter((filterUser)=>filterUser.address.city==='Los Angeles');
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
   return (
     <div id={'hw02'}>
-      <UserList2 users={currentUsers} filterUsers={filterUsers} />
+      <UserList2 users={currentUsers} filterUsers={filterUsers}/>
     </div>
   );
 };
--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -1,23 +1,20 @@
 import {CurrentUser} from './CurrentUser';
-import {UsersObjectType} from './HW2';
+import {UsersObjectType, UserType} from './HW2';
 
 type UserList2PropsType = {
-	users: any // пропиши типизацию
-	filterUsers: any // пропиши типизацию
+	users: UsersObjectType // пропиши типизацию
+	filterUsers: (filteredUsers: UserType) => void
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
 	return (
 		<div id={'hw02-users'}>
 			<h2>User List:</h2>
-
-			{/*<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>*/}
-
-			{/*<ul>*/}
-			{/*  {props.users.myFriends.map((user) => (*/}
-			{/*    <ВЫЗЫВАЕМ КОМПОНЕНТУ CurrentUser />*/}
-			{/*  ))}*/}
-			{/*</ul>*/}
+			<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
+			<ul>
+			  {props.users.myFriends.map((user) =><CurrentUser key={user.id} user={user}/>
+			  )}
+			</ul>
 		</div>
 	);
 };
