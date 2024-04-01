fb1c5f8e944306e1986f14f0fa9748401964922a
ekaterinapupeka <etymanskaya@bk.ru>
2024-04-01 22:19:36 +0300

    first commit

--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -3,14 +3,14 @@ import {UsersObjectType, UserType} from './HW2';
 
 type UserList2PropsType = {
 	users: UsersObjectType // пропиши типизацию
-	filterUsers: (filteredUsers: UserType) => void
+	filterUsers: ()=>void
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
 	return (
 		<div id={'hw02-users'}>
 			<h2>User List:</h2>
-			<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
+			<button id={'hw02-filter-button'} onClick={() => props.filterUsers()}>SHOW ME FRIENDS FROM LA</button>
 			<ul>
 			  {props.users.myFriends.map((user) =><CurrentUser key={user.id} user={user}/>
 			  )}
